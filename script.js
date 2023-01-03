d8cacf8faa7485c438424751e266f854b37055a5
Teslim Akinola <devteslim@gmail.com>
2023-01-03 20:41:49 +0100

    Improved the JS so a set of five rounds can be played but still on the console

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function getComputerChoice(){
     return options[random];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function game(playerSelection, computerSelection){
+   
    playerSelection = prompt('choose your weapon').toLowerCase();
    console.log(`Your Weapon is ${playerSelection}`)
    computerSelection = getComputerChoice();
@@ -12,27 +16,59 @@ function game(playerSelection, computerSelection){
 
    if(playerSelection === 'rock' && computerSelection === 'paper'){
         console.log('You Lose! Paper beats Rock');
+        computerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         console.log('You Win! Rock beats Scissors');
+        playerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else if ((playerSelection === 'rock' && computerSelection === 'rock')||(playerSelection === 'paper' && computerSelection === 'paper')||(playerSelection === 'scissors' && computerSelection === 'scissors')){
         console.log("It's a Tie");
+        playerScore++; computerScore++;
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else if (playerSelection === 'paper' && computerSelection === 'rock'){
         console.log('You Win! Paper beats Rock');
+        playerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
         console.log('You Lose! Scissors beats Paper');
+        computerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-    console.log('You Win! Scissors beats Paper');
+        console.log('You Win! Scissors beats Paper');
+        playerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
 
    }else {
-    console.log('You Lose! Rock beats Scissors');
+        console.log('You Lose! Rock beats Scissors');
+        computerScore++
+        console.log(`your score is ${playerScore} and computer is ${computerScore}`)
    }
    
 }
 
-game()
 
+function loop(){
+    let i = 0;
+   do{
+    game()
+    console.log('new round')
+    i++
+   }while(i<5)
+   if(playerScore > computerScore){
+    alert(`You Won!!! Final score is: your score ${playerScore} - ${computerScore} computer score`);
+}else {
+    alert(`You Lose!!! Final score is: your score ${playerScore} - ${computerScore} computer score`);
+}
+
+}
+
+
+
+
+loop()
